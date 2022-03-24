6d62713e126ab5787788a95d9e612e0656b3435a
Luis Barrios <20141112@lamolina.edu.pe>
2022-03-24 18:21:02 -0500

    newscripts
    update realized, new scripts for new petitions

--- reporte_comercial.R
+++ reporte_comercial.R
@@ -13,6 +13,11 @@ library(tidyr)
 vinculado<-read_xlsx("consejerovinculado.xlsx",sheet = "data",
                      col_types = c("numeric","numeric","text","numeric","text","numeric","text","text","text","numeric","text"))
 str(vinculado)
+unique(vinculado$`Fecha de vinculacion`)
+zonas<-unique(vinculado$Zona)
+zonas<-data.frame(zonas)
+zonas<-zonas %>% arrange(zonas)
+
 
 # damos formato a la fecha
 vinculado$`Fecha de vinculacion`<-as.Date(vinculado$`Fecha de vinculacion`, format = "%d/%m/%Y")
@@ -39,4 +44,43 @@ vinculado %>% filter(Coordinador!="NA")%>% filter(Perfil!="NA")%>%
 ggsave("coordinadormes.png",dpi = 300)
 
 
+vinculado %>% filter(Coordinador!="NA")%>% filter(Perfil!="NA")%>%
+  ggplot(aes(x=mes,y=Constante))+geom_bar(stat = "identity")+coord_flip()+theme_minimal()
 
+vinculado %>% filter(Coordinador!="NA")%>% filter(Perfil!="NA")%>%
+  ggplot()+geom_bar(aes(x=mes,y=Constante,fill=mes),stat = "identity")+
+  labs(title = "Consejeros vinculados por mes",x="",y="")+ theme_minimal()
+
+ggsave("consejerospormes.png",dpi = 700)
+
+
+vinculado %>% filter(Coordinador!="NA")%>% filter(Perfil!="NA")%>%
+  ggplot()+ geom_bar(aes(x=Zona))+coord_flip()
+
+#seleccionando para marzo
+zonas<-vinculado %>%filter(mes=="marzo") %>%  filter(Coordinador!="NA")%>% filter(Perfil!="NA")%>%
+  count(Zona) %>% arrange(-n)
+
+zona3meses<-vinculado  %>%  filter(Coordinador!="NA")%>% filter(Perfil!="NA")%>% group_by(mes) %>% 
+  count(Zona) %>% arrange(mes,-n)
+
+
+top20zonas<-zonas[1:20,]
+top40zonas<-zonas[1:40,]
+zona3meses[1:20,]
+subset()
+
+top20zonas %>% 
+  ggplot(aes(x=reorder(Zona,n),y=n))+ geom_bar(stat = "identity",fill="brown")+coord_flip()+
+  labs(title = "Top 20 consejeros por zona",x="",y="")
+
+top40zonas %>% 
+  ggplot(aes(x=reorder(Zona,n),y=n))+ geom_bar(stat = "identity",fill="brown")+coord_flip()+
+  labs(title = "Top 40 consejeros por zona en marzo",x="",y="")
+
+ggsave("top40zonas.png",dpi = 700)
+
+
+zona3meses %>% 
+  ggplot(aes(x=reorder(Zona,n),y=n))+ geom_bar(stat = "identity",fill="brown")+coord_flip()+
+  labs(title = "Top 40 consejeros por zona en marzo",x="",y="")+facet_grid(.~mes)
--- voximplant.R
+++ voximplant.R
@@ -10,9 +10,31 @@ library(tidyr)
 llamadas<-read_xlsx("voximplant.xlsx")
 
 str(llamadas)
+unique(llamadas$`Date of call start`)
+
+llamadas$`Date of call start`<-as.Date(llamadas$`Date of call start`)
+llamadas<-llamadas %>% mutate(mes=month(`Date of call start`))
+
+
+unique(llamadas$`Agent B`)
+unique(llamadas$`Agent A`)
+
 
 llamadas$`Call duration`<-as.numeric(llamadas$`Call duration`)
 llamadas$`Call cost`<-as.numeric(llamadas$`Call cost`)
 
 hist(llamadas$`Call duration`)
 hist(llamadas$`Call cost`)
+
+# solo marzo
+
+a<-llamadas %>% filter(mes=="3" & `Call duration`!="0") %>%  group_by(`Agent A`)%>%  count(`Is incoming`)
+
+a %>% ggplot(aes(x=reorder(`Agent A`,n),y=n))+
+  geom_bar(stat = "identity",fill="lightblue")+coord_flip()+facet_grid(.~`Is incoming`)
+
+ggsave("voximplant.png",dpi=800)
+
+juliocesar<-llamadas %>% filter(`Agent A`=="Julio Cesar Osorio")
+juliocesarsincero<-llamadas %>% filter(`Agent A`=="Julio Cesar Osorio" & `Call duration`!="0")
+
