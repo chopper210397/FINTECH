42c1d94def69167f2a45f3b5f8cd46f75cd64f24
Luis Barrios <20141112@lamolina.edu.pe>
2022-10-12 15:15:32 -0500

    update

--- c_sat_by_month_agent.R
+++ c_sat_by_month_agent.R
@@ -11,25 +11,45 @@ library(ggplot2)
 # name_of_sheets<-sheet_names("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing")
 
 
-nelson_rubio<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Nelson Rubio",
+nelson_rubio<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                         sheet = "Nelson Rubio",
                          col_types = "Dnccc")
 1  # este 1 es para seleccionar el perfil de google pre configurado, no borrar
-luz_dary_useche<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Luz Dary Useche",
+luz_dary_useche<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                            sheet = "Luz Dary Useche",
                             col_types = "Dnccc")
-jose_luis_alvarez<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Jose Luis Alvarez",
+jose_luis_alvarez<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                              sheet = "Jose Luis Alvarez",
                               col_types = "Dnccc")
-camilo_escobedo<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Camilo Escobedo",
+camilo_escobedo<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                            sheet = "Camilo Escobedo",
                             col_types = "Dnccc")
-tatiana_real<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Tatiana Real",
+tatiana_real<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                         sheet = "Tatiana Real",
                          col_types = "Dnccc")
-luisa_moreno<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Luisa Moreno",
+luisa_moreno<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                         sheet = "Luisa Moreno",
                          col_types = "Dnccc")
-leidy_rojas<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Leidy Rojas",
+leidy_rojas<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                        sheet = "Leidy Rojas",
                         col_types = "Dnccc")
-karen_fernandez<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Karen Fernandez",
+karen_fernandez<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                            sheet = "Karen Fernandez",
                             col_types = "Dnccc")
-catalina_suarez<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Catalina Suarez",
+catalina_suarez<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                            sheet = "Catalina Suarez",
                             col_types = "Dnccc")
+
+# nuevos sheet agregados
+jose_jhiovanni<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                           sheet = "Jose Jhiovanni Vivas",
+                            col_types = "Dnccc")
+esmeralda_correa<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",
+                             sheet = "Esmeralda Correa",
+                           col_types = "Dnccc")
+andres_aguilar<-range_read("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing",sheet = "Andrés Aguilar",
+                             col_types = "Dnccc")
+
 
 
 # homogenizando los nombres de las columnas
@@ -41,6 +61,9 @@ colnames(luisa_moreno)<-colnames(nelson_rubio)
 colnames(leidy_rojas)<-colnames(nelson_rubio)
 colnames(karen_fernandez)<-colnames(nelson_rubio)
 colnames(catalina_suarez)<-colnames(nelson_rubio)
+colnames(jose_jhiovanni)<-colnames(nelson_rubio)
+colnames(esmeralda_correa)<-colnames(nelson_rubio)
+colnames(andres_aguilar)<-colnames(nelson_rubio)
 
 # creando columna nombre
 nelson_rubio<-nelson_rubio %>% mutate(nombre= 'Nelson Rubio')
@@ -52,9 +75,12 @@ luisa_moreno<-luisa_moreno %>% mutate(nombre='Luisa Moreno')
 leidy_rojas<-leidy_rojas %>% mutate(nombre='Leidy Rojas')
 karen_fernandez<-karen_fernandez %>% mutate(nombre='Karen Fernandez')
 catalina_suarez<-catalina_suarez %>% mutate(nombre='Catalina Suarez')
+jose_jhiovanni<-jose_jhiovanni %>% mutate(nombre='Jose Jhiovanni Vivas')
+esmeralda_correa<-esmeralda_correa %>% mutate(nombre='Esmeralda Correa')
+andres_aguilar<-andres_aguilar %>% mutate(nombre='Andrés Aguilar')
 
 # data total unida
-last_month<-bind_rows(
+historic_data<-bind_rows(
   nelson_rubio,
   luz_dary_useche,
   jose_luis_alvarez,
@@ -63,29 +89,25 @@ last_month<-bind_rows(
   luisa_moreno,
   leidy_rojas,
   karen_fernandez,
-  catalina_suarez
+  catalina_suarez,
+  jose_jhiovanni,
+  esmeralda_correa,
+  andres_aguilar
 )
-# creando mes con funciòn similar a date_trunc()
-last_month<-last_month %>% mutate(mes=trunc.Date(Timestamp,"months"))
-
-# creando base de datos por mes para todos los agentes segùn C-SAT promedio
-historic_data_by_agent<-last_month %>% group_by(mes,nombre) %>%
-  summarise(avg_calification=mean(`¿Cómo calificarías la atención que te dio Nelson de Aflore?`),
-            cantidad=n())
-
-# dibujo tal y como esta calculado en el dashboard servicios
-ggplot(historic_data_by_agent %>% group_by(mes) %>% summarise(avg_calification2=mean(avg_calification)),
-       aes(x=mes,y=avg_calification2,label=round(avg_calification2,1 ) ))+geom_line()+geom_label()
 
 
-# dibujo de C-SAT promedio por mes para todo el equipo (como esta en el ppt ComSac de caro para juriscoop)
-ggplot( last_month %>% group_by(mes) %>% summarise(avg_calification=mean(`¿Cómo calificarías la atención que te dio Nelson de Aflore?`)) ,
-        aes(x=mes,y=avg_calification,label=round(avg_calification,1 ))) + geom_line()  +
-  geom_label()  + labs(x="",y="",title = "C-SAT")
-
-
-
-#ss <- gs4_create("wish_contact_last_7_days", sheets = selected_data)
-
-sheet_write(historic_data_by_agent,ss="https://docs.google.com/spreadsheets/d/1CNdsejB5HRBYDAj6pajzfpjE60_BBSf2uA_KRWAkJPc/edit?usp=sharing",sheet="historic_data_by_agent")
+# escribiendo la data en un googlesheet que sirve como fuente para el dashboard
+sheet_write(historic_data,
+            ss="https://docs.google.com/spreadsheets/d/1CNdsejB5HRBYDAj6pajzfpjE60_BBSf2uA_KRWAkJPc/edit?usp=sharing",
+            sheet="historic_data_by_agent")
 rm(list=ls())
+
+# # dibujo de C-SAT promedio por mes para todo el equipo (como esta en el ppt ComSac de caro para juriscoop)
+# ggplot( last_month %>% group_by(mes) %>% summarise(avg_calification=mean(`¿Cómo calificarías la atención que te dio Nelson de Aflore?`)) ,
+#         aes(x=mes,y=avg_calification,label=round(avg_calification,1 ))) + geom_line()  +
+#   geom_label()  + labs(x="",y="",title = "C-SAT")
+
+
+
+
+
