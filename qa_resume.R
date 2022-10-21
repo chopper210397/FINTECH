925b3498a79f3e0f4f3067f02fc30d866a88795c
chopper032197 <luisbarrios2197@gmail.com>
2022-10-20 19:54:53 -0500

    update

--- qa_resume.R
+++ qa_resume.R
@@ -26,66 +26,70 @@ for (selected_agent in name_of_sheets) {
 # AHORA YA SE QUE SI ESCRIBO LOS DATA FRAMES MANUALMENTE SI LES PUEDO APLICAR 
 # FUNCIONES CON LAPPLY O UN FOR, EL PROBLEMA ES QUE LA FUNCIÓN COLNAMES() NO HACE EFECTO
 # POR LO TANTO HAY QUE BUSCAR OTRO CAMINO 
-df_list<-list( )
+# df_list<-list( )
+# 
+# lapply(df_list ,
+#        function(x) x %>% mutate(id="1") )
+# 
+# 
+# for (i in 1:length(df_list)) {
+#   df_list[[i]]<-df_list[[i]] %>% rename( = )
+# }
+# 
+# for (i in 1:length(df_list)) {
+#   print(names(df_list[[i]])<-c("Timestamp","Calificación","FCR","Comentario","Teléfono","Nombre"))
+#   
+# }
+# 
+# 
+# for (df in df_list) {
+#   colnames(df)<-c("Timestamp","Calificación","FCR","Comentario","Teléfono","Nombre")
+# }
 
-lapply(df_list ,
-       function(x) x %>% mutate(id="1") )
-
-
-for (i in 1:length(df_list)) {
-  df_list[[i]]<-df_list[[i]] %>% rename()
-}
-
-for (i in 1:length(df_list)) {
-  print(names(df_list[[i]])<-c("Timestamp","Calificación","FCR","Comentario","Teléfono","Nombre"))
-  
-}
-
-
-for (df in df_list) {
-  colnames(df)<-c("Timestamp","Calificación","FCR","Comentario","Teléfono","Nombre")
-}
-
+new_column_names<-c("Timestamp","Calificación","FCR","Comentario","Teléfono","Nombre")
 
 # homogenizando los nombres de las columnas
-colnames(luz_dary_useche)<-colnames(nelson_rubio)
-colnames(jose_luis_alvarez)<-colnames(nelson_rubio)
-colnames(camilo_escobedo)<-colnames(nelson_rubio)
-colnames(tatiana_real)<-colnames(nelson_rubio)
-colnames(luisa_moreno)<-colnames(nelson_rubio)
-colnames(leidy_rojas)<-colnames(nelson_rubio)
-colnames(karen_fernandez)<-colnames(nelson_rubio)
-colnames(catalina_suarez)<-colnames(nelson_rubio)
-colnames(jose_jhiovanni)<-colnames(nelson_rubio)
-colnames(esmeralda_correa)<-colnames(nelson_rubio)
-colnames(andres_aguilar)<-colnames(nelson_rubio)
+colnames(Andrés_Aguilar)<-new_column_names
+colnames(Camilo_Escobedo)<-new_column_names
+colnames(Catalina_Suarez)<-new_column_names
+colnames(Cristian_Castro)<-new_column_names
+colnames(Diego_Cruz)<-new_column_names
+colnames(Dulfay_Linares)<-new_column_names
+colnames(Esmeralda_Correa)<-new_column_names
+colnames(Jenny_Rincón)<-new_column_names
+colnames(Jorge_Fernández)<-new_column_names
+colnames(Jose_Jhiovanni_Vivas)<-new_column_names
+colnames(Jose_Luis_Alvarez)<-new_column_names
+colnames(Karen_Fernandez)<-new_column_names
+colnames(Leidy_Rojas)<-new_column_names
+colnames(Luisa_Moreno)<-new_column_names
+colnames(Luz_Dary_Useche)<-new_column_names
+colnames(Natalia_Piedrahita)<-new_column_names
+colnames(Nelson_Rubio)<-new_column_names
+colnames(Sandra_Herrera)<-new_column_names
+colnames(Tatiana_Real)<-new_column_names
+colnames(Yaneth_Murillo)<-new_column_names
 
-# creando columna nombre
-nelson_rubio<-nelson_rubio %>% mutate(nombre= 'Nelson Rubio')
-luz_dary_useche<-luz_dary_useche %>% mutate(nombre='Luz Dary Useche')
-jose_luis_alvarez<-jose_luis_alvarez %>% mutate(nombre='Jose Luis Alvarez')
-camilo_escobedo<-camilo_escobedo %>% mutate(nombre='Camilo Escobedo')
-tatiana_real<-tatiana_real %>% mutate(nombre='Tatiana Real')
-luisa_moreno<-luisa_moreno %>% mutate(nombre='Luisa Moreno')
-leidy_rojas<-leidy_rojas %>% mutate(nombre='Leidy Rojas')
-karen_fernandez<-karen_fernandez %>% mutate(nombre='Karen Fernandez')
-catalina_suarez<-catalina_suarez %>% mutate(nombre='Catalina Suarez')
-jose_jhiovanni<-jose_jhiovanni %>% mutate(nombre='Jose Jhiovanni Vivas')
-esmeralda_correa<-esmeralda_correa %>% mutate(nombre='Esmeralda Correa')
-andres_aguilar<-andres_aguilar %>% mutate(nombre='Andrés Aguilar')
-
-# data total unida
+# UNIENDO TODA LA DATA DE C-SAT (QA EXTERNO)
 historic_data<-bind_rows(
-  nelson_rubio,
-  luz_dary_useche,
-  jose_luis_alvarez,
-  camilo_escobedo,
-  tatiana_real,
-  luisa_moreno,
-  leidy_rojas,
-  karen_fernandez,
-  catalina_suarez,
-  jose_jhiovanni,
-  esmeralda_correa,
-  andres_aguilar
+  Andrés_Aguilar,
+  Camilo_Escobedo,
+  Catalina_Suarez,
+  Cristian_Castro,
+  Diego_Cruz,
+  Dulfay_Linares,
+  Esmeralda_Correa,
+  Jenny_Rincón,
+  Jorge_Fernández,
+  Jose_Jhiovanni_Vivas,
+  Jose_Luis_Alvarez,
+  Karen_Fernandez,
+  Leidy_Rojas,
+  Luisa_Moreno,
+  Luz_Dary_Useche,
+  Natalia_Piedrahita,
+  Nelson_Rubio,
+  Sandra_Herrera,
+  Tatiana_Real,
+  Yaneth_Murillo
 )
