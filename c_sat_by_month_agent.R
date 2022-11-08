f64dfb1207fdd204dc03930902ba3eb60e8905da
chopper032197 <luisbarrios2197@gmail.com>
2022-11-08 13:52:13 -0500

    token updated

--- c_sat_by_month_agent.R
+++ c_sat_by_month_agent.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(lubridate)
 library(tidyr)
 library(ggplot2)
-# token: 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
+# token: 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
 # tal parece que los nombres de los sheets no pueden contener tildes o falla la lectura mediante gs4
 
 # name_of_sheets<-sheet_names("https://docs.google.com/spreadsheets/d/12VXX9J2UixC3IWMwmwzoGXgRCWl8KoONXS2t_bEqeEM/edit?usp=sharing")
