0799da54560e093dd290869fce2257b58e440477
chopper032197 <luisbarrios2197@gmail.com>
2022-11-18 09:12:02 -0500

    se agregó el tipo de acompañamiento al correo

--- planes_de_accion_acompañamientos.R
+++ planes_de_accion_acompañamientos.R
@@ -17,7 +17,7 @@ nombres_hojas<-sheet_names("https://docs.google.com/spreadsheets/d/1hrHkd7Fy-rvt
 # reading the sheet
 data<-range_read("https://docs.google.com/spreadsheets/d/1hrHkd7Fy-rvtF9keyvtHKi9Po9HToY6vrutkI7qmsi4/edit?usp=sharing",
                  sheet = nombres_hojas,
-                 col_types = "DDccccccc")
+                 col_types = "DDcccccccc")
 
 # creando diccionario para correos
 correos <- c("Andrés Aguilar"="andresmauricio@aflore.co",
@@ -40,21 +40,21 @@ correos <- c("Andrés Aguilar"="andresmauricio@aflore.co",
 
 # selecting data from yesterday , be careful if is monday cause cant yesterday is not friday so app dont send those messages
 ifelse( as.character( max( data$Timestamp ) ) ==  as.character( today()-1),
-        data <- data %>% filter( as.character( Timestamp ) == as.character( today()-1 )),
+        data <- data %>% filter( as.character( Timestamp ) == as.character( today()-1)),
         0 )
 
-# data<-data[13,]
+data<-data[4,]
 
   created_message<-gm_mime() %>%
     gm_to( correos[data$Colaborador] ) %>% 
-    gm_cc("carolina.reyes@aflore.co,talento@aflore.co") %>%
-    gm_bcc("dora.giraldo@aflore.co") %>% 
+    gm_cc("carolina.reyes@aflore.co,dora.giraldo@aflore.co,talento@aflore.co") %>%
     gm_from("luis.barrios@aflore.co") %>%
     gm_subject(paste0("Plan de acción acompañamiento de ", data$Colaborador )) %>%
     gm_text_body(
       paste0( 
         "Hola ",data$Colaborador," !","\n",
         "Te envío el resumen del acompañamiento realizado el día ",data$`Fecha del acompañamiento`," junto a ",data$`Realiza el acompañamiento`,"\n","\n",
+        "Tipo de acompañamiento: ", "\t", data$`Tipo de acompañamiento` ,"\n",
         "Realizó el acompañamiento: ","\t", data$`Realiza el acompañamiento`,"\n",
         "Fecha de acompañamiento: ","\t", data$`Fecha del acompañamiento` ,"\n",
         "Plazo: ", "\t", as.Date(data$Plazo , "%m/%d/%Y") ,"\n", "\n",
