34f5842257e833904d3570dc3c5eb70cf5a5c18d
chopper032197 <luisbarrios2197@gmail.com>
2022-12-04 20:23:21 -0500

    update

--- comisiones_comsac.R
+++ comisiones_comsac.R
@@ -17,7 +17,7 @@ nda_data<-range_read("https://docs.google.com/spreadsheets/d/1xKqqAhjGRoU7HYXiHo
            col_types = "c_ccnnnnccccccnnnnnc_____n")
 1
 
-# sapply(qa_externo_data, class)
+# sapply(qa_interno_servicio, class)
 
 nda_data$`Date from`<-ymd_hms(nda_data$`Date from`,tz=Sys.timezone())
 
@@ -32,9 +32,17 @@ qa_externo_data<-range_read("https://docs.google.com/spreadsheets/d/1CNdsejB5HRB
 ############################ QA INTERNO ###########################
 ###################################################################
 # AUDITORIA INTERNA DE SERVICIO
-
+qa_interno_servicio<-range_read("https://docs.google.com/spreadsheets/d/1tYXEEq_34kxoKh0Vo4mfG5aUopC5wbFNyeTXqZqzsL4/edit?usp=sharing",
+           sheet = "Form Responses 1",
+           col_types = "TccccnncncncccccncDcccccc")
 # AUDITORIA INTERNA DE PQRSD
-
+qa_interno_pqrsd<-range_read("https://docs.google.com/spreadsheets/d/1XgXYMt4qjep1_L8JUNCzM7Snfqp5XtnfKEV3DA_4HjU/edit?usp=sharing",
+                                sheet = "Form Responses 1",
+                                col_types = "TcDccccccnnncnccccccc")
 # AUDITORIA INTERNA DE DESEMBOLS´OS
-
+qa_interno_desembolsos<-range_read("https://docs.google.com/spreadsheets/d/1QxJoshRIxD_8tUMMgEOwhD1zaRhWUDX48mcAmuaZMDk/edit?usp=sharing",
+                             sheet = "data")
 # AUDITORIA INTERNA DE COMERCIAL
+qa_interno_comercial<-range_read("https://docs.google.com/spreadsheets/d/1XLl8_naZa89kguxuEGAmbBq7R8tzdN30xncLY_ODxT0/edit?usp=sharing",
+                                   sheet = "Form Responses 1",
+                                 col_types = "TcccDccccnnccccccnccc")
